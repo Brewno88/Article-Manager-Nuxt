f8eba61f1318309810d1bb372b7df5da3a74ed2d
Vincenzo <ilcontevince@gmail.com>
2020-06-18 13:39:14 +0100

    implemented solutions to update likes

--- components/IncrementLikes.vue
+++ components/IncrementLikes.vue
@@ -1,7 +1,7 @@
 <template>
   <v-btn color="success" @click="incrementLikes">
     <v-icon color="red">mdi-heart</v-icon>
-    <span>{{ likes }}</span>
+    <span>{{ likes + tempLikes }}</span>
   </v-btn>
 </template>
 
@@ -17,14 +17,21 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      tempLikes: 0
+    }
+  },
   methods: {
     incrementLikes() {
+      this.$data.tempLikes++
       let tempObj = {}
       tempObj = JSON.parse(localStorage.getItem('articles'))
       // If in not in article page get article by its Index prop (from index page) else from the its route id
       this.$route.params.id === undefined
         ? tempObj[this.$props.index].likes++
         : tempObj[this.$route.params.id].likes++
+
       return localStorage.setItem('articles', JSON.stringify(tempObj))
     }
   }
